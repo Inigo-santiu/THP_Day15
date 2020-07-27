1e1efd89fd327e47b2c13d4e635b920218f71df4
Inigo-santiu <dstavroff@gmail.com>
2020-07-27 17:36:41 +0200

    up to 2.6

--- lib/controller.rb
+++ lib/controller.rb
@@ -4,8 +4,12 @@
 require 'gossip'
 
 class ApplicationController < Sinatra::Base
+  # get '/' do
+  #   erb :index # index is the file that replaces the HTML content here, this line for example: <html><body></body></html>
+  # end
+
   get '/' do
-    erb :index # <html><body></body></html>
+    erb :index, locals: {gossips: Gossip.all}
   end
 
   get '/gossips/new/' do
@@ -13,7 +17,18 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/gossips/new/' do
-    Gossip.new("super_auteur", "super gossip").save
+    Gossip.new(params["gossip_author"],params["gossip_description"]).save
+    redirect '/'
   end
+
+  # post '/gossips/new/' do
+  #   puts "Salut, je suis dans le serveur"
+  #   puts "This the content of the PARAMS hash : #{params}"
+  #   puts "This comes was added by user in gossip_author : #{params["gossip_author"]}"
+  #   puts "User added to gossip_content : #{params["gossip_content"]}"
+  #   puts "That's it.  All done!"
+  # end
+
+
   # run! if app_file == $0
-end 
+end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -14,5 +14,17 @@ attr_accessor :author, :description
       csv << [@author, @description]
     end
   end
+
+  def self.all
+    all_gossips = [] #This starts the empty array
+    CSV.read("./db/gossip.csv").each do |csv_line|  #Creates the gossips, line by line
+      all_gossips << Gossip.new(csv_line[0], csv_line[1]) # gossips that have just been created
+    end
+    return all_gossips
+  end
 #binding.pry
 end
+
+#REMEMBER : To activate 'self' in the pry, type Gossip.all
+#This is because 'self' is the same as the class name, and '.all' is the 
+# method that acts on the class.
